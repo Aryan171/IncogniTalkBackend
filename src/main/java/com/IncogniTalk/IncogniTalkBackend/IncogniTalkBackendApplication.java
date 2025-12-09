64b574d8daf29cd5beed48ae86e9046c207469ab
Aryan171 <62366692+Aryan171@users.noreply.github.com>
2025-12-09 16:33:47 +0530

    Initial commit

---
+++ src/main/java/com/IncogniTalk/IncogniTalkBackend/IncogniTalkBackendApplication.java
@@ -0,0 +1,13 @@
+package com.IncogniTalk.IncogniTalkBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncogniTalkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncogniTalkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IncogniTalk/IncogniTalkBackend/IncogniTalkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IncogniTalk.IncogniTalkBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncogniTalkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
